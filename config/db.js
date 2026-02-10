d0890068e24adf33796989d7bf637dc5aa96e451
hartik teetwal <162800633+hartikteetwal@users.noreply.github.com>
2026-02-10 18:17:40 +0530

    changes

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async()=>{
-    await mongoose.connect(`mongodb+srv://teetwalhartik:258012@cluster0.tthoz.mongodb.net/food-del`).then(()=>console.log("DB Connected"))
+    await mongoose.connect(process.env.MONGOOB_URI).then(()=>console.log("DB Connected"))
 }
